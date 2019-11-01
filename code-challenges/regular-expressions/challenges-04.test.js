3d990e436813e44d4d84626866a6a85f96c20b02
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-10-31 21:34:19 -0700

    completed challenge 7

--- code-challenges/regular-expressions/challenges-04.test.js
+++ code-challenges/regular-expressions/challenges-04.test.js
@@ -113,7 +113,8 @@ Hint: All of these words end with the letters "ells".
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
 const findShells = (str) => {
-  // Solution code here...
+  let myRegex = /(se)*a*s*h*e(lls)/g;
+  return str.match(myRegex);
 };
 
 /* ------------------------------------------------------------------------------------------------
