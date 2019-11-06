1cb5337625a797343a485850ad4228ca20b204f2
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-11-05 19:41:31 -0800

    completed challenge 4

--- code-challenges/array-map/challenges-07.test.js
+++ code-challenges/array-map/challenges-07.test.js
@@ -45,9 +45,7 @@ Read the MDN documentation on String.charCodeAt() if necessary.
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
-const charCode = (arr) => {
-  // Solution code here...
-};
+const charCode = arr => arr.map(index => index.charCodeAt());
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
