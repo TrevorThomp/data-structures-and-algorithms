59261f9cf150a0cf5076a4acd304ecb88e969ebe
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-11-13 19:06:56 -0800

    completed challenge 8

--- code-challenges/strMethod-every/challenges-13.test.js
+++ code-challenges/strMethod-every/challenges-13.test.js
@@ -101,9 +101,13 @@ Return a two-dimensional array with the same roster, but where anyone whose name
 For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again', 'still Brook']] returns [['Actual Person'], ['Human Person']]
 ------------------------------------------------------------------------------------------------ */
 
-const unenrollBrook = (arr) => {
-  // Solution code here...
-};
+const unenrollBrook = arr => arr
+  .map(item => {
+    return item.reduce((acc,curr) => {
+      !curr.includes('Brook') ? acc.push(curr) : false;
+      return acc
+    }, [])
+  })
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
